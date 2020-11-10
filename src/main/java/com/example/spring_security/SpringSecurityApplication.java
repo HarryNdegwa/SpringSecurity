963343c0dc61fd139b5044fbf6efa24eb27cb064
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-10 22:51:35 +0300

    Added security config class

--- src/main/java/com/example/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/example/spring_security/SpringSecurityApplication.java
@@ -1,13 +1,18 @@
 package com.example.spring_security;
 
+import com.example.spring_security.services.CustomUserDetailsService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringSecurityApplication.class, args);
+
+		// System.out.println(ctx);
 	}
 
 }
---
+++ src/main/java/com/example/spring_security/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.spring_security.security;
+
+import com.example.spring_security.services.CustomUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/helloo/admin").hasRole("ADMIN").antMatchers("/helloo")
+                .hasAnyRole("ADMIN", "USER").and().httpBasic();
+    }
+}
