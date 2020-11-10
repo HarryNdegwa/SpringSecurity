968d653f751875a2d6e6379c1ff9233e22e1f155
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-11-11 00:05:15 +0300

    Final, clone at your own risk

--- src/main/java/com/example/spring_security/SpringSecurityApplication.java
+++ src/main/java/com/example/spring_security/SpringSecurityApplication.java
@@ -1,18 +1,14 @@
 package com.example.spring_security;
 
-import com.example.spring_security.services.CustomUserDetailsService;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(SpringSecurityApplication.class, args);
+		SpringApplication.run(SpringSecurityApplication.class, args);
 
-		// System.out.println(ctx);
 	}
 
 }
